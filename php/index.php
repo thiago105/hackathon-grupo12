d3c6bc3d68e86e857bc9140c4895237af4b4a82a
gabriel <gabriel.h.friedrichsen@gmail.com>
2025-06-12 01:44:35 -0300

    Index feito porem sem conexão como banco de dados e sem a parte logica

---
+++ php/index.php
@@ -0,0 +1,41 @@
+<?php
+require_once "html/header.php";
+?>
+
+<body>
+
+ <div class="container mb-5 mt-3">
+        <div class="row">
+            <div class="col-12 col-md-8 offset-md-2"> 
+                <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="true">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="images/0066b3.png" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="images/images.png" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="images/40e0d0.png" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</body>
+<?php
+require_once "html/footer.php";
+?>
