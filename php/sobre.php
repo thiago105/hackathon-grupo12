4807d98b909e44d1fa093c2dc1bd474baaa0e57a
github.com/Hendreu04 <hendreu.itami@edu.unipar.br>
2025-06-12 17:08:19 -0300

    foi adicionado o conteúdo do 'sobre'

--- php/sobre.php
+++ php/sobre.php
@@ -2,7 +2,37 @@
 require_once "html/header.php";
 ?>
 <body>
+    <h1>Sobre a Faculdade ALFA Umuarama - UniALFA</h1>
+
+    <h4>História da Faculdade ALFA Umuarama - UniALFA</h4>
+
+    <img src="images/alfa.jpg" alt="Foto externa da IES">
+
+    <p>A Faculdade ALFA Umuarama - UniALFA, sustentada pela experiência de seus mantenedores, já atuantes na prestação de serviços educacionais há mais de vinte e cinco anos, tem como um de seus objetivos dar continuidade ao ensino com qualidade e contribuir com a formação de profissionais para o mercado de trabalho.<p> 
     
+    <p>Sempre atenta às mudanças e às necessidades de novas tecnologias e exigências de especialistas visa promover grandes possibilidades de ingresso no mercado empresarial.</p>
+
+    <p>A Faculdade ALFA Umuarama - UniALFA conta com uma equipe de professores qualificados, com experiência no ensino superior e no mercado de trabalho garantindo ao aluno o conhecimento necessário, responsabilidade e infra-estrutura adequada para alcançar o que há de melhor em sua área.</p>
+
+    <h5>Objetivos</h5>
+
+    <ul>
+        <li>Aliar informação com formação: garantir que os alunos tenham tanto o embasamento científico, fundamentação teórica, quanto o técnico, aplicabilidade na prática.</li>
+        <li>Formular programas que incorporem a identificação das competências críticas empresariais e humanas e reflitam o compromisso com a cidadania empresarial.</li>
+        <li>Introduzir modelos comportamentais voltados para o aprendizado permanente, de forma a proporcionar a contínua atualização dos conhecimentos. Oferecer cursos de acordo com o que o mercado pede, sendo isto identificado através de pesquisas.</li>
+        <li>Estender à comunidade as atividades acadêmicas, visando a elevação do nível sócio-econômico-cultural.</li>
+        <li>Promover o ambiente interno de desenvolvimento de relações interpessoais, propiciando o crescimento integrado do ser humano e o pleno exercício de suas habilidades e potencialidades.</li>
+        
+    </ul>
+
+    <h5>Missão</h5>
+
+    <p>Constituir-se em centro de excelência no campo do ensino superior, construindo uma educação comprometida com a ética, a cidadania e o conhecimento, resultando na formação de profissionais aptos a contribuírem no desenvolvimento da sociedade.</p>
+
+    <h5>Finalidades</h5>
+
+    <p>Fundamentada na sua filosofia, a Faculdade ALFA Umuarama - UniALFA tem por finalidade a solidificação das diretrizes curriculares emanadas do Ministério da Educação cumprindo os princípios didático-pedagógicos para os seus cursos. Essas diretrizes solidificarão a excelência das práticas acadêmicas a serem desenvolvidas no decorrer das graduações da Faculdade.</p>
+
 </body>
 <?php
 require_once "html/footer.php";
