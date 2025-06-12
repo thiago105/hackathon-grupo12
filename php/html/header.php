847d0e9b61fa35964eba99e1e1aaed37ffc93295
gabriel <gabriel.h.friedrichsen@gmail.com>
2025-06-11 23:35:01 -0300

    Adicionado link para o icons do bootstrap

--- php/html/header.php
+++ php/html/header.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous"
         defer>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"
         defer></script>
@@ -16,7 +17,8 @@
 </head>
 <header>
     <div class="row" id="navBar">
-        <div class="col-1" id="logo"><a href="index.php"><img src="images/logoUnialfa.png" alt="logo da Unialfa" width="180" height="67"></a></div>
+        <div class="col-1" id="logo"><a href="index.php"><img src="images/logoUnialfa.png" alt="logo da Unialfa"
+                    width="180" height="67"></a></div>
         <div class="col-10 d-flex justify-content-center align-items-center">
             <nav class="d-flex justify-content-center align-items-center">
                 <ul>
@@ -26,6 +28,7 @@
                 </ul>
             </nav>
         </div>
-        <div class="col-1 d-flex justify-content-center align-items-center" ><button type="button" class="btn" id="btnLogin">Login</button></div>
+        <div class="col-1 d-flex justify-content-center align-items-center"><button type="button" class="btn"
+                id="btnLogin">Login</button></div>
     </div>
 </header>
