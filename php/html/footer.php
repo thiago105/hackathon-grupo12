86900d9738f6ca8b8034df5e33c4452d0725eca1
gabriel <gabriel.h.friedrichsen@gmail.com>
2025-06-12 01:43:21 -0300

    Footer adicionado, falta terminar de colocar os Git-hubs dos integrantes

---
+++ php/html/footer.php
@@ -0,0 +1,53 @@
+<footer>
+    <div class="row">
+        <div class="col-12 d-flex justify-content-center align-items-center">
+            <h3>Desenvolvido por: Grupo 12</h3>
+        </div>
+        <div class="col-12"><br></div>
+        <div class="col-4 d-flex justify-content-center align-items-center">
+            <div>
+                <p><strong>Nome:</strong> Andre Mateus Roll <strong>RA:</strong>14547</p>
+                <p><strong>Nome:</strong> Eduardo Henrique Pereira Dos Santos <strong>RA:</strong>14565</p>
+                <p><strong>Nome:</strong> Thiago Santos <strong>RA:</strong> 240055</p>
+                <p><strong>Nome:</strong> Gabriel Henrique Friedrichsen <strong>RA:</strong>14552</p>
+                <p><strong>Nome:</strong> Hendreu Satosh Zampieri Itami <strong>RA:</strong>240061</p>
+            </div>
+        </div>
+        <div class="col-4">
+            <!--https://github.com/GHFriedrichsen-->
+            <div id="git">
+                <p><i class="bi bi-github"></i><a href="" target="_blank"> - </a></p>
+                <p><i class="bi bi-github"></i><a href="" target="_blank"> - </a></p>
+                <p><i class="bi bi-github"></i><a href="https://github.com/thiago105" target="_blank"> - thiago105</a></p>
+                <p><i class="bi bi-github"></i><a href="https://github.com/GHFriedrichsen" target="_blank"> - GHFriedrichsen</a></p>
+                <p><i class="bi bi-github"></i><a href="https://github.com/Hendreu04" target="_blank"> - Hendreu04</a></p>
+            </div>
+        </div>
+        <div class="col-4">
+            <p>
+                <i class="bi bi-geo-alt"></i>
+                <span>Av. Paraná 7327, Zona III,
+                    CEP 87502-000, Umuarama - PR</span>
+                <br><br>
+            </p>
+            <p id="whats">
+                <i class="bi bi-whatsapp"></i>
+                <span><a href="https://api.whatsapp.com/send?phone=554436222500"> (44) 36222500 WhatsApp</a></span>
+            </p>
+        </div>
+        <div class="col-12">
+            <br>
+            <p>
+                Faculdade ALFA Umuarama - UniALFA - 2025 - Todos os direitos reservados
+            </p>
+        </div>
+    </div>
+</footer>
+<!-- 
+ 
+- 
+ - 
+ - 
+-->
+
+</html>
