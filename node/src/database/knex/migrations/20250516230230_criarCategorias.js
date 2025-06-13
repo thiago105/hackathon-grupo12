c7ffa2fef357ebeac170187bfa7fffac75153dbe
GHFriedrichsen <gabriel.h.friedrichsen@gmail.com>
2025-06-13 19:48:00 -0300

    api node adicionada

---
+++ node/src/database/knex/migrations/20250516230230_criarCategorias.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('categorias', (table) => {
+    table.increments('id')
+    table.text('nome')
+    table.timestamp('created_at')
+      .defaultTo(knex.fn.now())
+    table.timestamp('updated_at')
+      .defaultTo(knex.fn.now())
+  }).then(() => {
+    console.log('Criado a tabela de Categoria')
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('categorias').then(() => {
+    console.log('Deletado tabela de categorias')
+  })
+};
